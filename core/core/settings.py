31117fe9b7a2afec7ded596be31a0397b5fc5bfc
Kheiripour <76651844+kheiripour@users.noreply.github.com>
2023-05-31 20:02:39 +0330

    Update settings.py, remove smtp credential

--- core/core/settings.py
+++ core/core/settings.py
@@ -206,8 +206,8 @@ EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = "kheiribusiness@gmail.com"
-EMAIL_HOST_PASSWORD = "olsaonsafhfqcblq"
+EMAIL_HOST_USER = # removed for security reason
+EMAIL_HOST_PASSWORD = # removed for security reason
 
 
 # EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
